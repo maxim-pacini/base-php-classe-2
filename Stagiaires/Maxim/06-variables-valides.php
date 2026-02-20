14399776d1e331ab8f13205497dd4a5b8a233976
maxim-pacini <maxim10pacini@gmail.com>
2026-02-20 15:20:55 +0100

    exercice6

---
+++ Stagiaires/Maxim/06-variables-valides.php
@@ -0,0 +1,11 @@
+<?php
+$variable = "valeurdelavariable";
+$variable2 = 123;
+$variable3 = 123.456;
+$estActif = true;
+$nombres = [1, 2, 3];
+$personne = ["clef1" => "valeur1", "clef2" => "valeur2"];
+$rien = null;
+
+// Permet d'afficher la valeur des variableso
+var_dump($variable, $variable2, $variable3,$estActif,$nombres,$personne);
