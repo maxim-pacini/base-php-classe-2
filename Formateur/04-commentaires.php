c40fc5f7eacf8bbbac690476f72006fb208db209
WebDevCF2m2026 <gitweb@cf2m.be>
2026-03-04 15:12:34 +0100

    04

---
+++ Formateur/04-commentaires.php
@@ -0,0 +1,13 @@
+<?php
+
+// commentaire sur une ligne
+
+# commentaire sur autre ligne
+
+// long commentaire sur une seule ligne, il faut donc faire attention à la numérotation car le wordwrap peut être activé (retour automatique à la ligne)
+
+/*
+commentaire 
+sur plusieures
+lignes
+*/
